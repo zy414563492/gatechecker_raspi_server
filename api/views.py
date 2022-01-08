d020225cb17893b668248d18fbc4544b5a826c34
zy414563492 <414563492@qq.com>
2022-01-09 00:08:27 +0900

    Update views.py

--- api/views.py
+++ api/views.py
@@ -148,13 +148,12 @@ def face(request):
         paylodmsg1 = "\"log_id\": \""
         paylodmsg2 = "\", \"temperature\":"
         paylodmsg3 = ", \"time\": \""
-        # paylodmsg4 = "\", \"is_blacklist\":"
-        # paylodmsg5 = ", \"to_device\": \""
+        paylodmsg4 = "\", \"is_blacklist\":"
+        paylodmsg5 = ", \"to_device\": \""
         paylodmsg6 = "\"}"
-        paylodmsg = "{}{}{}{}{}{}{}{}".format(
+        paylodmsg = "{}{}{}{}{}{}{}{}{}{}{}{}".format(
             paylodmsg0, paylodmsg1, rand_str(4), paylodmsg2, temperature, paylodmsg3,
-            # str(machine_date), paylodmsg4, "False", paylodmsg5, cid, paylodmsg6)
-            str(machine_date), paylodmsg6)
+            str(machine_date), paylodmsg4, False, paylodmsg5, cid, paylodmsg6)
 
         # paylodmsg = {
         #     "log_id": rand_str(4),

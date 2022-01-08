cd4ebc494249b452c43a8af80e77c8926fc7e12a
zy414563492 <414563492@qq.com>
2022-01-08 19:19:01 +0900

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     # 下面是类视图名
     path('', views.IndexView.as_view(), name='index'),
     url(r'face', views.face, name='face'),
-    url(r'zytest', views.zytest, name='zytest'),
+    # url(r'zytest', views.zytest, name='zytest'),
     # url(r'face/hankvision', views.hankvision, name='hankvision'),
 ]
